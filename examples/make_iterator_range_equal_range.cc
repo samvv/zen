7f8d8b4749a993f1013a6890a5c4a4e675985fe7
Sam Vervaeck <samvv@pm.me>
2025-12-25 19:18:22 +0100

    Change map to multimap in examples/make_iterator_range_equal_range.cc

--- examples/make_iterator_range_equal_range.cc
+++ examples/make_iterator_range_equal_range.cc
@@ -5,7 +5,7 @@
 #include "zen/range.hpp"
 
 int main() {
-  std::unordered_map<int, int> map { { 1, 1 }, { 2, 2 } };
+  std::unordered_multimap<int, int> map { { 1, 1 }, { 2, 2 }, { 2, 3} };
   for (auto [k, v]: zen::make_iterator_range(std::as_const(map).equal_range(2))) {
     std::cerr << k << " = " << v << "\n";
   }
